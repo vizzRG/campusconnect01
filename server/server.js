0858ee66c0cb1e23b0b5aff90aaeb84e09a8c533
Vishal Tyagi <vizzrg@gmail.com>
2026-03-12 11:34:04 +0530

    changes in env file

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ import userRoutes from "./routes/userRoutes.js";
 import aiRoutes from "./routes/aiRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
 
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
