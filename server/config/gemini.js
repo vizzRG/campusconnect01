645b4f7920bb564d6c1469a167e6b610131643dd
Vishal Tyagi <vizzrg@gmail.com>
2026-03-14 01:37:37 +0530

    fixed minor bug of gemini api

--- server/config/gemini.js
+++ server/config/gemini.js
@@ -1,5 +1,8 @@
 // server/config/gemini.js
 import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
 
 const genAI = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
